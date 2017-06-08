95a81337269de6217d2be361fbe144b474d643e9
deelaka96 <deelakaalgawatta@gmail.com>
2017-06-08 11:37:15 +0530

    Update sign.php

--- sign.php
+++ sign.php
@@ -7,7 +7,7 @@ $pwd=$_POST['pwd'];
 $enc_pwd=md5($pwd);
 $index_no=$_POST['index'];
 $sql = "INSERT INTO user_details(name,user_name,password,email,index_no)
-VALUES ('$name','$user_name','$enc_pwd','$email','$nic','$index_no')";
+VALUES ('$name','$user_name','$enc_pwd','$email','$index_no')";
 if($conn->query($sql)==TRUE){
     header('location:login.php');
 }
